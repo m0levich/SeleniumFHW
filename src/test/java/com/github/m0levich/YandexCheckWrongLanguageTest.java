f898d1e4f84ff234d45dd664cc767677f8a453b3
Egor Molkov <molkov@i-teco.ru>
2020-05-02 15:18:50 +0300

    + message in the assert

--- src/test/java/com/github/m0levich/YandexCheckWrongLanguageTest.java
+++ src/test/java/com/github/m0levich/YandexCheckWrongLanguageTest.java
@@ -9,31 +9,31 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.assertj.core.api.Assertions.*;
 
 import java.util.concurrent.TimeUnit;
 
-public class SeleniumHW {
-private WebDriver webDriver;
+public class YandexCheckWrongLanguageTest {
+    private WebDriver webDriver;
+
     @BeforeMethod
-    public void initDriver(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+    public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         webDriver = new ChromeDriver();
     }
 
     @Test
-    public void seleniumHW(){
+    public void changeOfLanguage() {
         webDriver.get("https://www.yandex.ru/");
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement input = webDriver.findElement(By.name("text"));
-        input.sendKeys("руддщ ццщкдв",Keys.ENTER);
-        assertThat(webDriver.getTitle()).contains("hello world");
+        input.sendKeys("руддщ ццщкдв", Keys.ENTER);
+        Assert.assertTrue(webDriver.getTitle().contains("hello world"), "Title not contains \"hello world\"");
         WebElement expectedField = webDriver.findElement(By.name("text"));
-        Assert.assertEquals(expectedField.getAttribute("value"),"hello world");
+        Assert.assertEquals(expectedField.getAttribute("value"), "hello world", "Value failed");
     }
 
     @AfterMethod
-    public void closeDriver(){
+    public void closeDriver() {
         webDriver.quit();
     }
 }
